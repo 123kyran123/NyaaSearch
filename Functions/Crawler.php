3ebe12227846c19058fb585e9fd26f123d273d9e
123kyran123 <openbaarkylefransen@gmail.com>
2016-03-27 18:23:48 +0200

    Added files via upload

--- Functions/Crawler.php
+++ Functions/Crawler.php
@@ -83,7 +83,7 @@
 	}
 	
 	function getTorrentInfo($td){
-		$groupInfo = Array();
+		$torrentInfo = Array();
 		
 		//Get the group of the show
 		if(strpos(substr($td->innertext, 0, 1), "[") !== true){	
@@ -97,16 +97,63 @@
 				if(stripos($group, "batch") === false){
 					//Remove the [ tag from the group name
 					$group = explode("[", $group)[1];	
-				
-					//Fill Array with groups
-					$groupInfo[] = $group;
+
+					//Fill Array with group
+					$torrentInfo[] = $group;
+					
+					
+					//Get Name of the show
+					$name = $td->innertext;
+
+					//Strip the name from the string
+					$name = explode("]", strip_tags($name))[1];
+					$name = explode("[", $name)[0];
+					
+					//Check if there isn't a divider before the number and remove the number
+					$name = explode("0", $name)[0];
+		
+					//Check if the text contains a certain character and remove these
+					if(stripos($name, "_") !== false){
+						$name = str_replace('_', ' ', $name);
+					}
+					
+					//Remove the episode number and other information
+					$name = explode(" - ", $name)[0];
+					
+					//Change string to lower case except first letter
+					$name = ucwords(strtolower($name));
+					
+					//Fill Array with torrent name
+					$torrentInfo[] = $name;
+					
+					//Get episode number or if it's a batch
+					$episode = $td->innertext;
+					$episode = explode("]", strip_tags($episode))[1];
+					
+					//Check if the text contains a certain character and remove these
+					if(stripos($episode, "_") !== false){
+						$episode = str_replace('_', ' ', $episode);
+					}
+					
+					//Remove show title from $episode
+					if(stripos($episode, "-") !== false){
+						$episode = explode("- ", $episode)[1];
+					}
+					
+					//Check if it actually has an episode number
+					if(is_numeric(substr($episode, 0, 1))){
+						
+						//Remove everything behind episode number
+						if(stripos($episode, "[") !== false){
+							$episode = explode(" [", $episode)[0];
+						} else if(stripos($episode, "(") !== false){
+							$episode = explode(" (", $episode)[0];
+						}
+						
+						echo $episode."<br/>";
+					}
+					
 				}
-				
-				//Get Name of the show
-				$name = $td->innertext;
-				$name = explode("]", strip_tags($name))[1];
-				echo $name."<br/>";
-				
 			}
 		}
 		
